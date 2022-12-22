cf2a7a814eb5b662946aab784d86642889372635
jonaed1230 <jonaed.com.bd@gmail.com>
2022-12-23 00:41:11 +0600

    feat(major): added link part

---
+++ neuralnine/main.py
@@ -0,0 +1,20 @@
+from textblob import TextBlob
+from newspaper import Article
+import nltk
+nltk.download('punkt')
+
+url = 'https://en.wikipedia.org/wiki/Mathematics'
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+text = article.summary
+
+print(text)
+
+blob = TextBlob(text)
+sentiment = blob.sentiment.polarity
+
+print(sentiment)

ed4f604a7ce9f6197227cac12a684092388b017f
jonaed1230 <jonaed.com.bd@gmail.com>
2022-12-30 01:32:01 +0600

    feat(common): added text support for text analysis

--- neuralnine/main.py
+++ neuralnine/main.py
@@ -2,19 +2,21 @@ from textblob import TextBlob
 from newspaper import Article
 import nltk
 nltk.download('punkt')
-
-url = 'https://en.wikipedia.org/wiki/Mathematics'
-article = Article(url)
-
-article.download()
-article.parse()
-article.nlp()
-
-text = article.summary
-
-print(text)
-
-blob = TextBlob(text)
-sentiment = blob.sentiment.polarity
-
-print(sentiment)
+choice = input("Do you want to enter a URL or a text? (URL/text): ")
+if choice.lower() == "url":
+    url = input("Enter the URL: ")
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+    text = article.summary
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+    print("Your result for this article is: " + str(sentiment))
+elif choice.lower() == "text":
+    text = input("Enter the text: ")
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+    print("Your result for this text is: " + str(sentiment))
+else:
+    print("Invalid choice!")
